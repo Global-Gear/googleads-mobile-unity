b6e60b44becac897dc333b32bb0b09dad73531bc
髙沢 <takazawa-y@global-gear.biz>
2022-03-29 15:28:51 +0900

    フォルダ名変更に伴う調整

--- source/plugin/Assets/GoogleMobileAdsCustom/Editor/PListProcessor.cs
+++ source/plugin/Assets/GoogleMobileAdsCustom/Editor/PListProcessor.cs
@@ -31,7 +31,7 @@ public static class PListProcessor
 
     private const string KEY_SK_ADNETWORK_ID = "SKAdNetworkIdentifier";
 
-    private const string SKADNETWORKS_RELATIVE_PATH = "GoogleMobileAds/Editor/GoogleMobileAdsSKAdNetworkItems.xml";
+    private const string SKADNETWORKS_RELATIVE_PATH = "GoogleMobileAdsCustom/Editor/GoogleMobileAdsSKAdNetworkItems.xml";
 
     private const string SKADNETWORKS_FILE_NAME = "GoogleMobileAdsSKAdNetworkItems.xml";
 
